b961a40c7fa890dcec60c0357e6dd372e70b2240
Adam Kecskes <adam.k@powerednow.com>
2020-12-09 00:13:50 +0100

    Import only the needed utilities from `mathjs`

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,13 +1,11 @@
 import iterate, { algorithms } from './index';
 
 test('iterate', () => {
-    expect(iterate(
-        1,
-        algorithms.MA,
-        {
+    expect(
+        iterate(1, algorithms.MA, {
             Z: { re: 0, im: 0 },
             C: { re: 0, im: 0 },
-            exp: 2
-        }
-    )).toEqual({ reValues: [0], imValues: [0] });
+            exp: 2,
+        }),
+    ).toEqual({ reValues: [0], imValues: [0] });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as math from "mathjs";
-import { Complex } from "mathjs";
+import { add, multiply, pow, complex } from 'mathjs';
+import { Complex } from 'mathjs';
 
 type ValueOf<T> = T[keyof T];
 
@@ -7,18 +7,15 @@ export type AlgorithmParameters = { Z: Complex; C: Complex; exp: number };
 
 export const algorithms = {
     MA({ Z, C, exp }: AlgorithmParameters): Complex {
-        return math.add(math.pow(Z, exp), C) as Complex;
+        return add(pow(Z, exp), C) as Complex;
     },
 
     MB({ Z, C, exp }: AlgorithmParameters): Complex {
-        return math.pow(math.add(math.pow(Z, 1 / exp), C), exp) as Complex;
+        return pow(add(pow(Z, 1 / exp), C), exp) as Complex;
     },
 
     MC({ Z, C, exp }: AlgorithmParameters): Complex {
-        return math.pow(
-            math.add(math.pow(math.multiply(Z, C), 1 / exp), C),
-            exp
-        ) as Complex;
+        return pow(add(pow(multiply(Z, C), 1 / exp), C), exp) as Complex;
     },
 };
 
@@ -34,17 +31,11 @@ export type IterationOptions = {
     exp: number;
 };
 
-export default function iterate(
-    iterations: number,
-    algorithm: Algorithm,
-    options: IterationOptions
-): IterationResult {
+export default function iterate(iterations: number, algorithm: Algorithm, options: IterationOptions): IterationResult {
     const values: IterationResult = { reValues: [], imValues: [] };
 
-
-
-    let Z: Complex = math.complex(options.Z.re, options.Z.im);
-    const C: Complex = math.complex(options.C.re, options.C.im);
+    let Z: Complex = complex(options.Z.re, options.Z.im);
+    const C: Complex = complex(options.C.re, options.C.im);
 
     for (let n: number = 0; n < iterations; n += 1) {
         Z = algorithm({
